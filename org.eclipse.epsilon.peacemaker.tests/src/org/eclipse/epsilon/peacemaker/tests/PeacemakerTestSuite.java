61ba20726561fe8eb4ee8844fdf2b3db8e254a1a
alfonsodelavega <alfonso.delavega@tuta.io>
2021-02-17 11:45:53 +0000

    Add combined test suite

---
+++ org.eclipse.epsilon.peacemaker.tests/src/org/eclipse/epsilon/peacemaker/tests/PeacemakerTestSuite.java
@@ -0,0 +1,13 @@
+package org.eclipse.epsilon.peacemaker.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+		ComicTests.class,
+		EMFCompareTests.class })
+public class PeacemakerTestSuite {
+
+}
