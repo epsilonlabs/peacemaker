da3eebe629d325b3ffeb072f2d977d282535737d
alfonsodelavega <alfonso.delavega@tuta.io>
2021-03-12 11:15:15 +0000

    Tidy plugin dependencies

--- org.eclipse.epsilon.peacemaker/src/org/eclipse/epsilon/peacemaker/dt/PeacemakerPlugin.java
+++ org.eclipse.epsilon.peacemaker/src/org/eclipse/epsilon/peacemaker/dt/PeacemakerPlugin.java
@@ -1,13 +1,39 @@
 package org.eclipse.epsilon.peacemaker.dt;
 
-import org.eclipse.epsilon.common.dt.AbstractEpsilonUIPlugin;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
 
-public class PeacemakerPlugin extends AbstractEpsilonUIPlugin {
+public class PeacemakerPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.epsilon.peacemaker";
 
+	// The shared instance
+	private static PeacemakerPlugin plugin;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
 	public static PeacemakerPlugin getDefault() {
-		return (PeacemakerPlugin) plugins.get(PeacemakerPlugin.class);
+		return plugin;
 	}
 }
